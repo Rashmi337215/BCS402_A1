69de88e49dc18691f78c0453c68c8b23a4e57200
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-10 19:18:12 +0530

    Que5.asm

--- Que5.asm
+++ Que5.asm
@@ -1,25 +1,10 @@
-        AREA SWAP_XOR, CODE, READONLY
-ENTRY 
-
-        LDR     r0, =num1        ; Load address of num1
-        LDR     r1, =num2        ; Load address of num2
-
-        LDR     r2, [r0]         ; r2 = num1
-        LDR     r3, [r1]         ; r3 = num2
-
-        EOR     r2, r2, r3       ; r2 = r2 ^ r3
-        EOR     r3, r2, r3       ; r3 = r2 ^ r3 = (r2 ^ r3) ^ r3 = r2
-        EOR     r2, r2, r3       ; r2 = r2 ^ r3 = (r2 ^ r3) ^ r2 = r3
-
-        STR     r2, [r0]         ; Store new value in num1
-        STR     r3, [r1]         ; Store new value in num2
-
-end
-        B       end              ; Infinite loop to end program
-
-        AREA SWAP_DATA, DATA, READWRITE
-
-num1    DCD     15              ; First number
-num2    DCD     42              ; Second number
-
+        AREA PROGRAM5, CODE, READONLY
+START
+	MOV r0,#5           ; Load first number(A=5)into r0
+	MOV r1,#10          ; Load second number(B=10)into r1
+	                    ; Swap r0 and r1 without using a temporary register
+	EOR r0,r0,r1        ; r0=r0^r1
+	EOR r1,r0,r1        ; r1=r1^r1 (now r1 holds original r0)
+	EOR r0,r0,r1        ; r0=r0^r1(now r0 holds original r1)
+here b here 
         END
