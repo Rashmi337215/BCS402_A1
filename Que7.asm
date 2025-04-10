a56d380f378ff03957ec68dd86b0c520e0f173b4
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-10 19:56:39 +0530

    Que7.asm

---
+++ Que7.asm
@@ -0,0 +1,8 @@
+         AREA PROGRAM7, CODE, READONLY 
+ENTRY
+         LDR R0,=0x10        ; Load the immediate value 0x10 into register R0
+	       MSR CPSR_c,R0       ; Move the value in R0 to the CPSR(changing processor mode)
+	       MRS R1,CPSR         ; Move the current CPSR value into R1(read status register)
+	       MOV R2,R1           ; Copy the CPSR value from R1 to R2
+HERE B HERE                  ; Infinite loop to halt execution
+         END 
