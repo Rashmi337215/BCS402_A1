ad5a35047129e2c8776b3828ce79bb0366b8f3fe
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-10 19:22:41 +0530

    Que2.asm

--- Que2.asm
+++ Que2.asm
@@ -1,28 +1,21 @@
         AREA FIBONACCI, CODE, READONLY
-ENTRY
-
-        MOV     r0, #0      
-        MOV     r1, #1          
-        LDR     r4, =fib_array  
-        MOV     r5, #10         
-        MOV     r6, #0          
-
-store_loop
-        CMP     r6, #10        
-        BEQ     end             
-        STR     r0, [r4], #4    
-
-        ADD     r2, r0, r1      
-        MOV     r0, r1         
-        MOV     r1, r2          
-        ADD     r6, r6, #1      
-        B       store_loop    
-
-end
-        B       end          
-
-        AREA FIB_DATA, DATA, READWRITE
-fib_array
-        SPACE   40            
-
+START
+        LDR R0, =0x40000000
+        MOV R1, #0
+        MOV R2, #1
+        MOV R5, #0x0A
+        STRB R1, [R0]
+        STRB R2, [R0, #1]
+        ADD R0, R0, #2
+LOOP
+        ADD R3, R2, R1
+        MOV R1, R2
+        MOV R2, R3
+        STRB R3, [R0]
+        ADD R0, R0, #1
+        SUB R5, R5, #1
+        CMP R5, #0
+        BNE LOOP
+HERE
+        B HERE
         END
